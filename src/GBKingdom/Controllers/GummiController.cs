b25167cb49e80447454fae4c9777adcd0ca1d9d9
David Wilson <davidtheadmiral@gmail.com>
2017-08-04 12:01:15 -0700

    updated project.json, add views, try to run page. laptop issues, will switch computers.

---
+++ src/GBKingdom/Controllers/GummiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GBKingdom.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GBKingdom.Controllers
+{
+    public class GummiController : Controller
+    {
+        // GET: /<controller>/
+        private GBKingdomContext db = new GBKingdomContext();
+        public IActionResult Index()
+        {
+            return View(db.Gummies.ToList());
+        }
+    }
+}
